fd14f911d9d0d947686da0f61de5682d48b7781f
lezhnevdaniil <97900835+lezhnevdaniil@users.noreply.github.com>
2022-04-04 16:02:07 +0300

    Delete src directory

--- src/services/UserService.tsx
+++
@@ -1,7 +0,0 @@
-import api from '../http/index';
-
-export default class UserService {
-  static async ActionGetAppointments() {
-    return api.get(`/allAppoints?token=${localStorage.getItem('token')}`);
-  }
-}
